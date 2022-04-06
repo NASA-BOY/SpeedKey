14fa9d54239f4daca3344c9908e4338934597ff4
NASA-BOY <itay.oliel9999@gmail.com>
2022-04-06 18:28:38 +0300

    edited main asm file and added some images
    made the main asm file ready to stat coding and added the main home screen image and key animation images.

--- SpeedKey.asm
+++ SpeedKey.asm
@@ -1,17 +1,19 @@
+; ==========SpeedKey by ITAY==========
 IDEAL
 MODEL small
+jumps
 STACK 100h
+P186
 DATASEG
-; --------------------------
-; Your variables here
-; --------------------------
+
+; Variables
+
 CODESEG
 start:
 	mov ax, @data
 	mov ds, ax
-; --------------------------
-; Your code here
-; --------------------------
+
+	;The Code
 	
 exit:
 	mov ax, 4c00h
